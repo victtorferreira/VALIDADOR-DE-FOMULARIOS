0b1973f845637f41baed5206b1db32b7854f57b9
victtorferreira <victojhou@gmail.com>
2023-08-27 15:52:58 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,70 +1,76 @@
-let validation = {
-  handleSubmit: (event) => {
-    event.preventDefault();
-    let send = true;
+let B7Validador = {
+    handleSubmit:(event)=>{
+        event.preventDefault()
+let send = true;
 
-    let inputs = form.querySelectorAll("input");
-
-    validation.clearErrors();
-    
-    for (let i = 0; i < inputs.length; i++) {
-      let input = inputs[i];
-      let check = validation.checkInput(input);
-      if (check !== true) {
+let inputs = form.querySelectorAll('input')
+B7Validador.clearErrors();
+for(let i=0;i<inputs.length; i++){
+    let input = inputs[i];
+    let check = B7Validador.checkInput(input);
+    if(check !== true){
         send = false;
-        validation.showError(input, check);
-      }
+        B7Validador.showError(input, check);
+
     }
+   
+}
 
-
-    if(send) {
-      form.submit();
-    }
-  },
-  checkInput: (input) => {
-    let rules = input.getAttribute("data-rules");
-
-    if (rules !== null) {
-      rules = rules.split('|');
-      for (let k in rules) {
-        let rDatails = rules[k].split("=");
-        switch (rDatails[0]) {
-          case 'required':
-            if(input.value == '') {
-                return 'Campo não pode ser vazio';
+if(send){
+    form.submit()
+}
+ },
+ checkInput:(input) => {
+    let rules = input.getAttribute('data-rules');
+    if(rules !== null){
+        rules = rules.split('|');
+        for(let k in rules){
+            let rDetails = rules[k].split('=');
+            switch(rDetails[0]){
+                case 'required':
+                    if(input.value == ''){
+                        return 'O campo não pode estar vazio.';
+                    }
+                break;
+                case 'min':
+                    if(input.value.length < rDetails[1]){
+                        return 'Campo precisa ter pelo menos '+rDetails[1]+' caracteres.'                   }
+                    break;
+                    case 'email':
+                        if(input.value != ''){
+                             let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                             if(!regex.test(input.value.toLowerCase())){
+                                return 'Esse email não é valido.'
+                             }
+                        }
+                    break
             }
-
-            break
-          case 'min':
-            break;
         }
-      }
     }
-
     return true;
-  },
-  showError:(input, error) => {
-    input.style.borderColor = 'red';
+ },
+ 
+    showError: (input, error) =>{
+    input.style.borderColor = '#FF0000';
 
     let errorElement = document.createElement('div');
     errorElement.classList.add('error');
     errorElement.innerHTML = error;
 
     input.parentElement.insertBefore(errorElement, input.ElementSibling);
-  },
+    },
+    clearErrors:()=>{
+        let inputs = form.querySelectorAll('input');
+        for(let i=0;i<inputs.length;i++){
+            inputs[i].style = ''
+        }
 
-  clearErrors:() => {
-    let inputs = form.querySelectorAll('input');
-    for(let i=0;i<inputs.length;i++) {
-        inputs[i].style = '';
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0;i<errorElements.length;i++){
+            errorElements[i].remove();
+        }
     }
-
-    let errorElements = document.querySelectorAll('.error');
-    for(let i=0;i<errorElements.length;i++) {
-        errorElements[i].remove();
-    }
-  }
 };
 
-let form = document.querySelector(".validator");
-form.addEventListener("submit",validation.handleSubmit);
+let form = document.querySelector('.validador')
+form.addEventListener('submit', B7Validador.handleSubmit);
